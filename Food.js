748f1ee4e3552eb05fff00ac5fc2577227dafc23
CadenAlford <alfordcaden@gmail.com>
2021-05-03 22:41:22 -0400

    js update

--- Food.js
+++ Food.js
@@ -3,13 +3,33 @@ para = document.getElementsByClassName("paras");
 iButton = document.getElementsByClassName("textButton");
 rButton = document.getElementsByClassName("rTextButton");
 
-iButton.addEventListener("click",onButtonClick);
+for (var i = 0; i< iButton.length; i++){
+    iButton[i].addEventListener('click', onButtonClick);
+}
+
+for (var i = 0; i< iButton.length; i++){
+    rButton[i].addEventListener('click', onRButtonClick);
+}
+
+
 rButton.addEventListener("click", onRButtonClick);
 
 function onButtonClick(){
-    paragraph.style.fontSize = "2.0em";
+    console.log("hi");
+
+    for (var p= 0; p < para.length; p++){
+        para[p].style.fontSize = "2.0em";
+        para[p].style.lineHeight= "1.7em";
+    }
+
 }
 
 function onRButtonClick(){
-    para.style.fontSize = "1.0em";
+    console.log("working");
+
+    for (var p= 0; p < para.length; p++){
+        para[p].style.fontSize = "1.0em";
+        para[p].style.lineHeight= "1.5em";
+    }
 }
+
